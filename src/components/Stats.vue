c8c895e209f866e3fca2d1eea964a19667feb4d5
EuronRamadanii <e.ramadani@digitbusiness.ch>
2022-10-20 11:39:33 +0200

    icons

--- src/components/Stats.vue
+++ src/components/Stats.vue
@@ -3,7 +3,10 @@
         <div class="gallery-inner container py-md-5 py-3">
             <div class="row stats-con">
                 <div class="col-lg-3 col-6 stats_info counter_grid">
-                    <span class="fa fa-laptop"></span>
+                    <!-- <span class="fa fa-laptop"></span> -->
+                    <div class="icon">
+                        <img style="height: 100px; width: 100px;" src="../assets/icons/project-management.png" alt="">
+                    </div>
                     <!-- <p class="counter">1200</p>
                      -->
                     <div class="iCountUp">
@@ -13,7 +16,10 @@
                     <h4 class="countup">Projects & Residentials Completed in 2020</h4>
                 </div>
                 <div class="col-lg-3 col-6 stats_info counter_grid1">
-                    <span class="fa fa-users"></span>
+                    <!-- <span class="fa fa-users"></span> -->
+                    <div class="icon">
+                        <img style="height: 100px; width: 100px;" src="../assets/icons/team.png" alt="">
+                    </div>
                     <!-- <p class="counter">2120</p> -->
                     <div class="iCountUp">
                         <ICountUp :startVal="stat2.startVal" :endVal="stat2.endVal" :decimals="stat2.decimals"
@@ -22,7 +28,10 @@
                     <h4 class="countup">Qualified Employees & Workers With Us</h4>
                 </div>
                 <div class="col-lg-3 col-6 stats_info counter_grid mt-lg-0 mt-5">
-                    <span class="fa fa-trophy"></span>
+                    <!-- <span class="fa fa-trophy"></span> -->
+                    <div class="icon">
+                        <img style="height: 100px; width: 100px;" src="../assets/icons/target.png" alt="">
+                    </div>
                     <!-- <p class="counter">1012</p> -->
                     <div class="iCountUp">
                         <ICountUp :startVal="stat3.startVal" :endVal="stat3.endVal" :decimals="stat3.decimals"
@@ -31,7 +40,10 @@
                     <h4 class="countup">Satisfied Clients We Served Globally</h4>
                 </div>
                 <div class="col-lg-3 col-6 stats_info counter_grid2 mt-lg-0 mt-5">
-                    <span class="fa fa-smile-o"></span>
+                    <!-- <span class="fa fa-smile-o"></span> -->
+                    <div class="icon">
+                        <img style="height: 100px; width: 100px;" src="../assets/icons/practice.png" alt="">
+                    </div>
                     <!-- <p class="counter">60</p> -->
                     <div class="iCountUp">
                         <ICountUp :startVal="stat4.startVal" :endVal="stat4.endVal" :decimals="stat4.decimals"
@@ -141,5 +153,9 @@ export default {
 
 .countup {
     color: #ffffff;
+}
+
+.icon {
+    margin: 0 0 20px 20px;
 }
 </style>
